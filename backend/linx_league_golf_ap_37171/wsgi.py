42f4f49432403c3e8abc2a1e06ce2afa1ad9faca
Crowdbotics <team@crowdbotics.com>
2022-09-28 16:13:29 +0000

    Initial commit.

---
+++ backend/linx_league_golf_ap_37171/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for linx_league_golf_ap_37171 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'linx_league_golf_ap_37171.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "linx_league_golf_ap_37171",
+  url: "https://linx_league_golf_ap_37171.botics.co",
+  api: "https://linx_league_golf_ap_37171.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
