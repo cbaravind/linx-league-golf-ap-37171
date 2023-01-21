7dd845747640c27fac0737035798394c0d1699d6
Sainyam Chandra <sainyam.chandra@crowdbotics.com>
2023-01-21 11:39:43 +0100

    refactor: added user filter for friends api

--- backend/friends/views.py
+++ backend/friends/views.py
@@ -6,6 +6,9 @@ from .serializers import CreateFriendSerializer, GetFriendSerializer
 
 class FriendViewSet(viewsets.ModelViewSet):
     queryset = Friend.objects.all()
+    filterset_fields = {
+        "user__id": ["exact"],
+    }
 
     def get_serializer_class(self):
         if self.request.method in ("PUT", "POST", "PATCH"):
