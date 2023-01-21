27e21d01a552cc6744fde8c9a690e45d96fd009d
Sainyam Chandra <sainyam.chandra@crowdbotics.com>
2023-01-21 12:09:27 +0100

    fix: append friends

--- backend/friends/serializers.py
+++ backend/friends/serializers.py
@@ -12,6 +12,19 @@ class CreateFriendSerializer(WritableNestedModelSerializer):
         fields = "__all__"
 
 
+class PatchFriendSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Friend
+        fields = "__all__"
+
+    def update(self, instance, validated_data):
+        print(validated_data)
+        for friend in validated_data.get("friends"):
+            instance.friends.add(friend)
+        instance.save()
+        return instance
+
+
 class GetFriendSerializer(serializers.ModelSerializer):
     friends = UserSerializer(many=True)
 
--- backend/friends/views.py
+++ backend/friends/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 
 from .models import Friend
-from .serializers import CreateFriendSerializer, GetFriendSerializer
+from .serializers import (
+    CreateFriendSerializer,
+    GetFriendSerializer,
+    PatchFriendSerializer,
+)
 
 
 class FriendViewSet(viewsets.ModelViewSet):
@@ -11,7 +15,9 @@ class FriendViewSet(viewsets.ModelViewSet):
     }
 
     def get_serializer_class(self):
-        if self.request.method in ("PUT", "POST", "PATCH"):
+        if self.request.method in ("PUT", "POST"):
             return CreateFriendSerializer
+        elif self.request.method == "PATCH":
+            return PatchFriendSerializer
         else:
             return GetFriendSerializer
