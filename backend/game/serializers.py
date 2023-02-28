56d7c40f2a99ba1a6e04bf8149fc77d60a867bdf
Zohaib Abbas <zohaibabbasza@outlook.com>
2023-03-01 00:07:16 +0500

    game fix

--- backend/game/serializers.py
+++ backend/game/serializers.py
@@ -20,19 +20,6 @@ class GameSerializerPOST(serializers.ModelSerializer):
         model = models.Game
         exclude = ('user', )
     
-    def create(self,validated_data):
-        request = self.context.get('request', None)
-        user = request.user
-        players = validated_data.pop('players')
-        data = []
-        for player in players:
-            game = models.Game.objects.create(
-                **validated_data
-            )
-            game.players.add(player)
-            data.append(game)
-        return game
-        
         
 
 class GameScoreSerializer(serializers.ModelSerializer):
