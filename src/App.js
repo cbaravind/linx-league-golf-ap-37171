03fed87468ca32a0d4caf70ed73d1e00fcbaec89
Aravind M <aravind.m.nga@crowbotics.com>
2023-04-14 18:41:19 +0530

    add new container wrapper

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {PersistGate} from 'redux-persist/integration/react';
 const {persistor, store} = configureStore();
 import RNBootSplash from 'react-native-bootsplash';
 import {SafeAreaView, StatusBar} from 'react-native';
+import colors from './themes/colors';
 export default class App extends React.Component {
   render() {
     return (
@@ -16,8 +17,11 @@ export default class App extends React.Component {
           <NavigationContainer
             onReady={() => RNBootSplash.hide({fade: true, duration: 1000})} //hide the splash screen once the navigation container and all children have finished mounting
           >
-            <StatusBar barStyle="light-content" backgroundColor={'black'} />
-            <SafeAreaView style={{backgroundColor: 'black'}} />
+            <StatusBar
+              barStyle="light-content"
+              backgroundColor={colors.gradientTop}
+            />
+            <SafeAreaView style={{backgroundColor: colors.gradientTop}} />
             <Navigator />
           </NavigationContainer>
         </PersistGate>
