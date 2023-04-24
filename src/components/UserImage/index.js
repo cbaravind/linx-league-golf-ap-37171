d98b10b8411b20634588b882ecbe1a334d5cfd03
Aravind M <aravind.m.nga@crowbotics.com>
2023-04-24 16:18:32 +0530

    Fixes userimage component
    3 sizes for using it throughout the app

--- src/components/UserImage/index.js
+++ src/components/UserImage/index.js
@@ -7,14 +7,32 @@ import {
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 
-export default function UserImage({imgurl, hasBorder, imgSize}) {
+export default function UserImage({imgurl, hasBorder, imgSize = 'medium'}) {
+  //imgSize will be available in 3 sizes - small,medium,x-large;default will be medium
   return (
-    <View style={[styles.imageWrapper, {borderWidth: !hasBorder ? 0 : 2}]}>
+    <View
+      style={[
+        styles.imageWrapper,
+        imgSize === 'small'
+          ? styles.smallImageWrapper
+          : imgSize === 'medium'
+          ? styles.mediumImageWrapper
+          : imgSize === 'x-large'
+          ? styles.largeImageWrapper
+          : undefined,
+        {borderWidth: !hasBorder ? 0 : 2},
+      ]}>
       <Image
         resizeMode="cover"
         style={[
           styles.imageStyle,
-          {width: imgSize || wp('11%'), height: imgSize || wp('11%')},
+          imgSize === 'small'
+            ? styles.smallImage
+            : imgSize === 'medium'
+            ? styles.mediumImage
+            : imgSize === 'x-large'
+            ? styles.largeImage
+            : undefined,
         ]}
         source={imgurl || images.dummyUser}
       />
@@ -23,13 +41,35 @@ export default function UserImage({imgurl, hasBorder, imgSize}) {
 }
 
 const styles = StyleSheet.create({
-  imageWrapper: {
+  smallImage: {
+    width: wp('9.5%'),
+    height: wp('9.5%'),
+  },
+  smallImageWrapper: {
+    width: wp('10.5%'),
+    height: wp('10.5%'),
+  },
+  mediumImage: {
+    width: wp('11%'),
+    height: wp('11%'),
+  },
+  mediumImageWrapper: {
     width: wp('12%'),
     height: wp('12%'),
+  },
+  largeImage: {
+    width: wp('22%'),
+    height: wp('22%'),
+  },
+  largeImageWrapper: {
+    width: wp('23%'),
+    height: wp('23%'),
+  },
+  imageWrapper: {
     borderWidth: 2,
     borderColor: colors.lightGreen,
-    borderRadius: 25,
+    borderRadius: 50,
     alignItems: 'center',
   },
-  imageStyle: {width: wp('11%'), height: wp('11%'), borderRadius: 25},
+  imageStyle: {borderRadius: 50},
 });
